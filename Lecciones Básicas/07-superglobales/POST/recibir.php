a791fe10d1b007199bbf9369bca2c4dde9aa5de0
Cris Morales LL <125212295+CrisMll@users.noreply.github.com>
2024-01-30 11:37:21 +0100

    Delete Lecciones Básicas directory

--- Lecciones Básicas/07-superglobales/POST/recibir.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "<h1>".$_POST['nombre']."</h1>"; //recibiríamos 'nombre' y mostramos
-echo "<h1>".$_POST['apellidos']."</h1>"; //los apellidos
-echo "<h1>".$_POST['telefono']."</h1>"; //el telefono
-
-var_dump($_POST); //muestra los valores recibidos por POST
-?>
